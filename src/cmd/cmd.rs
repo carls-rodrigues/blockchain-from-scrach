3a1a3483ca8c67b6417ac5525b50770248528d1d
Carlos Rodrigues <cerf@furg.br>
2024-04-01 18:17:56 -0300

    move cmd to its own file

---
+++ src/cmd/cmd.rs
@@ -0,0 +1,38 @@
+use crate::cmd;
+
+pub fn run_cmd() {
+    let command = clap::Command::new("Tbb")
+        .version("1.0")
+        .subcommand_required(true)
+        .about("Does awesome things")
+        .subcommand(cmd::balances_cmd())
+        .subcommand(cmd::tx_cmd())
+        .get_matches();
+
+    match command.subcommand() {
+        Some(("balances", args)) => {
+            let subcommand = args.subcommand();
+            if subcommand.is_none() {
+                println!("Balances command, no subcommand");
+                return;
+            }
+            if let Some(("list", _)) = subcommand {
+                cmd::get_database_state_from_disk();
+                println!("Balances command, list subcommand");
+            }
+        }
+        Some(("tx", args)) => {
+            let subcommand = args.subcommand();
+            if subcommand.is_none() {
+                println!("Transaction command, no subcommand");
+                return;
+            }
+            if let Some(("add", tx_args)) = subcommand {
+                cmd::add_new_tx(tx_args);
+            }
+        }
+        _ => {
+            println!("No command");
+        }
+    }
+}
--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,4 +1,6 @@
 mod balances;
+mod cmd;
 mod tx;
 pub use balances::*;
+pub use cmd::run_cmd;
 pub use tx::*;
